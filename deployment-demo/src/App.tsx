e030874f1b4076f05ad4e38cc6548298fa059502
Alex Mickelson <alex.mickelson@snow.edu>
2023-09-13 15:21:20 -0600

    started working on making the generic modal smart as a class

--- deployment-demo/src/App.tsx
+++ deployment-demo/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { CustomModal } from "./components/CustomModal";
 import { BookList } from "./features/bookList/BookList";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="container-md">
-      <CustomModal></CustomModal>
+      <button onClick={() => setShowModal((s) => !s)}>showmodal</button>
+      {showModal && (
+        <CustomModal
+          onChange={setShowModal}
+        >
+          <div>This is my modal body</div>
+          <div>other</div>
+        </CustomModal>
+      )}
       <BookList></BookList>
     </div>
   );
--- deployment-demo/src/components/CustomModal.tsx
+++ deployment-demo/src/components/CustomModal.tsx
@@ -1,10 +1,79 @@
-import { useState } from "react";
+import { Modal } from "bootstrap";
+import { FC, ReactNode, useEffect, useState } from "react";
 
-export const CustomModal = () => {
-  const [show, setShow] = useState(false);
+
+
+export const CustomModal: FC<{
+  children: ReactNode;
+  onChange: (s: boolean) => void;
+}> = ({ children, onChange }) => {
+  const [show, setShow] = useState(true);
+  const [isInTransition, setIsInTransition] = useState(false);
+
+  useEffect(() => {
+    const modalElement = document.getElementById("exampleModal");
+    if (!modalElement) return;
+
+    const bootstrapModalObject = Modal.getOrCreateInstance(modalElement);
+    bootstrapModalObject.show();
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      onChange(true);
+      setIsInTransition(true);
+    };
+    const modalElement = document.getElementById("exampleModal");
+    if (!modalElement) return;
+    modalElement.addEventListener("show.bs.modal", callback);
+
+    return () => {
+      modalElement.removeEventListener("show.bs.modal", callback);
+    };
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      setIsInTransition(false);
+    };
+    const modalElement = document.getElementById("exampleModal");
+    if (!modalElement) return;
+    modalElement.addEventListener("shown.bs.modal", callback);
+
+    return () => {
+      modalElement.removeEventListener("shown.bs.modal", callback);
+    };
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      setIsInTransition(true);
+    };
+    const modalElement = document.getElementById("exampleModal");
+    if (!modalElement) return;
+    modalElement.addEventListener("hide.bs.modal", callback);
+
+    return () => {
+      modalElement.removeEventListener("hide.bs.modal", callback);
+    };
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      onChange(false);
+      setIsInTransition(false);
+    };
+    const modalElement = document.getElementById("exampleModal");
+    if (!modalElement) return;
+    modalElement.addEventListener("hidden.bs.modal", callback);
+
+    return () => {
+      modalElement.removeEventListener("hidden.bs.modal", callback);
+    };
+  }, []);
   return (
     <>
-      <button
+      {/* <button
         type="button"
         className="btn btn-primary"
         data-bs-toggle="modal"
@@ -12,7 +81,7 @@ export const CustomModal = () => {
         onClick={() => setShow(true)}
       >
         Launch demo modal
-      </button>
+      </button> */}
       <div
         className={"modal fade"}
         id="exampleModal"
@@ -33,7 +102,7 @@ export const CustomModal = () => {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body">...</div>
+            <div className="modal-body">{children}</div>
             <div className="modal-footer">
               <button
                 type="button"
